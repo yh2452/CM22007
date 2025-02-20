58c404b2649d77d375416e3de75c050415bac8b9
Jamie Yeung <jamiethy@gmail.com>
2025-02-20 20:39:07 +0000

    backend code pt.1

--- backend.py
+++ backend.py
@@ -9,34 +9,81 @@ USER DATABASE
 --> access socials a user is planning to attend
     --> mark events accordingly when users first select to 'attend' (similar for 'unattend')
 --> access events users have previously pinned
-    --> mark events as 'pinned' for users (are we storing eventID or something inside the user database?)
+    --> mark events as 'pinned' for users
 --> access followed societies
     --> mark societies as 'followed' when users first select to 'follow' (similar for 'unfollow')
 '''
 
-def getAttendedEvents(userCursor, eventCursor, userid, flag):
+### [ATTENDED TABLE] ###
+# An 'Attended' Table where we store (userid, eventid)
+
+def toggleAttend(cursor, userid, eventid):
+    """
+    Allows a user to mark or unmark an event for future attending.
+    """
+    cursor.execute("SELECT eventid FROM Attended WHERE userid = (?) AND eventid = (?)", (userid, eventid))
+    event = cursor.fetchall()
+    if not event:
+        cursor.execute("INSERT INTO Attended VALUES (?,?)",(userid, eventid))
+    else:
+        cursor.execute("DELETE FROM Attended WHERE userid = (?) AND eventid = (?)", (userid, eventid))
+
+def getAttendedEvents(cursor, userid, flag):
     """
     Return all socials the user has attended in the past, or ones they are planning to attend.
 
     flag: TRUE = past attended events, FALSE = future events they plan to attend
     """
     date = datetime.datetime.now()
-
+    
     if flag:
-        userCursor.execute("SELECT eventid? FROM User? WHERE eventdate? < "+date)  #CHECK
+        cursor.execute("SELECT Event.* FROM Event INNER JOIN Attended ON Event.eventid=Attended.eventid WHERE Attended.userid = (?) AND Event.date < (?)", (userid, date))
     else:
-        userCursor.execute("SELECT eventid? FROM User? WHERE eventdate? >= "+date)  #CHECK
+        cursor.execute("SELECT Event.* FROM Event INNER JOIN Attended ON Event.eventid=Attended.eventid WHERE Attended.userid = (?) AND Event.date >= (?)", (userid, date))
 
-    events = userCursor.fetchall()
-    info = []
+    return cursor.fetchall()  # unless we want to format each result somehow
 
-    for eventid in events:
-        eventCursor.execute("SELECT eventinfo1, eventinfo2...? FROM Event? WHERE eventid? = "+eventid)  #CHECK
-        info.append(eventCursor.fetchall())
+### [PINNED TABLE] ###
+# A 'Pinned' Table where we store (userid, eventid)
 
-    # format info?
-    return info
+def togglePin(cursor, userid, eventid):
+    """
+    Allows a user to pin or unpin an event.
+    """
+    cursor.execute("SELECT eventid FROM Pinned WHERE userid = (?) AND eventid = (?)", (userid, eventid))
+    event = cursor.fetchall()
+    if not event:
+        cursor.execute("INSERT INTO Pinned VALUES (?,?)", (userid, eventid))
+    else:
+        cursor.execute("DELETE FROM Pinned WHERE userid = (?) AND eventid = (?)", (userid, eventid))
 
+def getPinnedEvents(cursor, userid):
+    """
+    Returns all events the user has pinned.
+    """
+    cursor.execute("SELECT Event.* FROM Event INNER JOIN Pinned ON Event.eventid=Pinned.eventid WHERE Attended.userid = (?)", (userid,)) 
+    return cursor.fetchall()  # unless we want to format each result somehow
+
+### [FOLLOWED TABLE] ###
+# A 'Followed' Table where we store (userid, societyid)
+
+def toggleFollow(cursor, userid, socid):
+    """
+    Allows a user to follow or unfollow a society.
+    """
+    cursor.execute("SELECT societyid FROM Followed WHERE userid = (?) AND societyid (?)", (userid, socid))
+    soc = cursor.fetchall()
+    if not soc:
+        cursor.execute("INSERT INTO Followed VALUES (?,?)", (userid, socid))
+    else:
+        cursor.execute("DELETE FROM Followed WHERE userid = (?) AND societyid (?)", (userid, socid))
+
+def getFollowed(cursor, userid):
+    """
+    Returns all societies the user follows.
+    """
+    cursor.execute("SELECT Society.* FROM Society INNER JOIN Followed ON Society.societyid=FOLLOWED.societyid WHERE Followed.userid = "+userid)
+    return cursor.fetchall()  # unless we want to format each result somehow
 
 '''
 SOCIAL DATABASE
